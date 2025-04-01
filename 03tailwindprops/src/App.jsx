33242a4fe55b5c7f61139ae5fb05ecc84a18f7a4
Pradeep Singh <51873457+psingh100298@users.noreply.github.com>
2025-04-01 21:06:09 +0530

    bg changer

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Components/Card";
+
+
+const App = () => {
+
+  return (
+   <>
+     <h1 className="bg-green-400 text-black p-4 rounded-xl">
+    Hello world!
+  </h1>
+    <Card subject="Science of Chemstry"/>
+    <Card subject="Science of Physics"/>
+    </>
+  );
+};
+
+export default App;
